55c3b6775a8365a4bcd0950f652b931ed098a985
ANDREE MATEO NARVAEZ ORTIZ <andreenarvaezmo@unimagdalena.edu.co>
2023-08-01 18:04:38 -0500

    clase 66

--- bases/src/app/dbz/pages/main-page.component.ts
+++ bases/src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
 
 @Component({
     selector: 'app-dbz-main-page',
@@ -6,4 +8,23 @@ import { Component} from '@angular/core';
 })
 export class MainPageComponent {
     constructor() { }
+
+    public characters: Character[] =[{
+        name:'Krillin',
+        power: 1000
+    },{
+        name:'Goku',
+        power: 9500
+    },{
+        name:'Vegetta',
+        power: 7500
+    }];
+
+    onNewCharacter(character: Character):void{
+        this.characters.push(character);
+    }
+
+    onDeleteCharacter(index:number):void{
+        this.characters.splice(index,1);
+    }
 }
